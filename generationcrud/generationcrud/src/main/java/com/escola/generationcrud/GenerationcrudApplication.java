c39882165d64b44e5562c3875dc14ae751b1091e
laisegalvao <lg.souza15@gmail.com>
2020-12-11 12:53:53 -0300

    AtvidadeCrud Escola

---
+++ generationcrud/generationcrud/src/main/java/com/escola/generationcrud/GenerationcrudApplication.java
@@ -0,0 +1,13 @@
+package com.escola.generationcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationcrudApplication.class, args);
+	}
+
+}
---
+++ generationcrud/generationcrud/src/main/java/com/escola/generationcrud/repository/TurmaRepository.java
@@ -0,0 +1,16 @@
+package com.escola.generationcrud.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.escola.generationcrud.model.Turma;
+
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long>{
+	
+	public List<Turma> findAllByTurmaContainingIgnoreCase (String turma);
+
+}
